cdf6d2ee47ebeb9208ca49854813747b0c51ec98
vsRevaL <mate531@freemail.hu>
2022-03-20 18:40:32 +0100

    asd2

--- blazor/BlazorTutorial/EmployeeManagement.Web/obj/Debug/net6.0/RazorDeclaration/Pages/DatabindingDemo.razor.g.cs
+++ blazor/BlazorTutorial/EmployeeManagement.Web/obj/Debug/net6.0/RazorDeclaration/Pages/DatabindingDemo.razor.g.cs
@@ -83,7 +83,7 @@ using EmployeeManagement.Web.Shared;
 #line hidden
 #nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/DataBindingDemo")]
-    public partial class DataBindingDemo : DataBindingDemoBase
+    public partial class DatabindingDemo : DataBindingDemoBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
